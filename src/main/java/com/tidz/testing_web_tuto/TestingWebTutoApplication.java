9762d9648d3e54e7c39ffdb17aaab8067d0c66e5
Tiago Gomes <tidgomes@gmail.com>
2024-10-07 11:31:06 -0300

    first commit

---
+++ src/main/java/com/tidz/testing_web_tuto/TestingWebTutoApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.testing_web_tuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWebTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/testing_web_tuto/TestingWebTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.testing_web_tuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
